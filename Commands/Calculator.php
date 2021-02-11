8ed80ab400205fa93938bc38e8b25dfa25f49e2e
Ayagan  Aidynuly <ayn_aid@bk.ru>
2021-02-11 16:19:35 +0000

    Update Calculator.php

--- Commands/Calculator.php
+++ Commands/Calculator.php
@@ -1,16 +1,16 @@
 <?php
-$TS_TER = array(
+$TS_TER = [
     'Алматинская область'               =>  1.78,
     'Южно-Казахстанская область'        =>  1.01,
     'Восточно-Казахстанская область'    =>  1.96,
     'Костанайская область'              =>  1.95,
-);
-$TS_TYPE = array(
+];
+$TS_TYPE = [
     'Легковые' =>                2.09,
     'Грузовые' =>                3.98,
     'Троллейбусы, трамваи'=>     2.33,
-);
-$BONUS_MALUS = array(
+];
+$BONUS_MALUS = [
     'Класс М' => 2.45,
     'Класс 0' => 2.30,
     'Класс 1' => 1.55,
@@ -26,7 +26,7 @@ $BONUS_MALUS = array(
     'Класс 11' => 0.60,
     'Класс 12' => 0.55,
     'Класс 13' => 0.50
-);
+];
 
 
 
@@ -40,27 +40,35 @@ $MRP = 2778;
 $age = intval(getAge($iin));
 $territory = getRegion($tsNum);
 
-$territory_coef = $TS_TER[$territory];
-$type_coef = $TS_TYPE[$typeCar];
-$driverLicense_coef = 0;
-$vehicleYear_coef = 0;
-$bonusMalus_coef = $BONUS_MALUS[$bonusMalus];
+$territoryCoef = $TS_TER[$territory];
+$typeCoef = $TS_TYPE[$typeCar];
+$driverLicenseCoef = 0;
+$vehicleYearCoef = 0;
+$bonusMalusCoef = $BONUS_MALUS[$bonusMalus];
 $experience = getExperience($dateDriverLicense);
 
 if ($age < 25) {
-    if ($experience < 2) $driverLicense_coef = 1.10;
-    else $driverLicense_coef = 1.05;
+    if ($experience < 2) {
+        $driverLicenseCoef = 1.10;
+    } else {
+        $driverLicenseCoef = 1.05;
+    }
 }
 else {
-    if ($experience < 2) $driverLicense_coef = 1.05;
-    else $driverLicense_coef = 1.00;
+    if ($experience < 2) {
+        $driverLicenseCoef = 1.05;
+    } else {
+        $driverLicenseCoef = 1.00;
+    }
 }
 
 $vehicleYear = 7;
 
 if ($vehicleYear <= 7) {
-    $vehicleYear_coef = 1.00;
-}else $vehicleYear_coef = 1.10;
+    $vehicleYearCoef = 1.00;
+} else {
+    $vehicleYearCoef = 1.10;
+}
 
 
 function getRegion($t)
@@ -68,10 +76,18 @@ function getRegion($t)
     $first = intval(substr($t,8,9));
     $region = "";
     switch ($first){
-        case 1: $region = "Алматинская область"; break;
-        case 2: $region = "Костанайская область"; break;
-        case 3: $region = "Южно-Казахстанская область"; break;
-        case 16: $region = "Восточно-Казахстанская область"; break;
+        case 1: 
+            $region = "Алматинская область"; 
+            break;
+        case 2: 
+            $region = "Костанайская область"; 
+            break;
+        case 3: 
+            $region = "Южно-Казахстанская область"; 
+            break;
+        case 16: 
+            $region = "Восточно-Казахстанская область"; 
+            break;
         // Continue...
     }
     return $region;
@@ -80,34 +96,34 @@ function getAge($i)
 {
     $time = date('Y-m-d');
 
-    $first_time = substr($i, 0, 2);
-    $second_time = substr($i, 2, 2);
-    $third_time = substr($i, 4, 2);
+    $firstTime = substr($i, 0, 2);
+    $secondTime = substr($i, 2, 2);
+    $thirdTime = substr($i, 4, 2);
 
-    if ($first_time > 21) {
-        $first_final = "19" . "" . $first_time . "-" . $second_time . "-" . $third_time;
-        $origin = new DateTime($first_final);
-        $tar = new DateTime($time);
-        $ans = $origin->diff($tar);
-        return $ans->format('%y');
+    if ($firstTime > 21) {
+        $firstFinal = "19" . "" . $firstTime . "-" . $secondTime . "-" . $thirdTime;
+        $origin = new DateTime($firstFinal);
+        $now = new DateTime($time);
+        $answer = $origin->diff($now);
+        return $answer->format('%y');
     } else {
-        $first_final = "20" . "" . $first_time . "-" . $second_time . "-" . $third_time;
-        $origin = new DateTime($first_final);
-        $tar = new DateTime($time);
-        $ans = $origin->diff($tar);
-        return $ans->format('%y');
+        $firstFinal = "20" . "" . $firstTime . "-" . $secondTime . "-" . $thirdTime;
+        $origin = new DateTime($firstFinal);
+        $now = new DateTime($time);
+        $answer = $origin->diff($now);
+        return $answer->format('%y');
     }
 }
 function getExperience($i)
 {
     $time = date('d.m.Y');
     $origin = new DateTime($time);
-    $tar = new DateTime($i);
-    $ans = $tar->diff($origin);
-    return $ans->format('%y');
+    $now = new DateTime($i);
+    $answer = $now->diff($origin);
+    return $answer->format('%y');
 }
 
 
-$ans = $MRP * $territory_coef * $type_coef * $driverLicense_coef * $vehicleYear_coef * $bonusMalus_coef;
+$answer = $MRP * $territoryCoef * $typeCoef * $driverLicenseCoef * $vehicleYearCoef * $bonusMalusCoef;
 
-echo $ans;
+echo $answer;
